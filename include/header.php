8f8504608c5f22a900a52601553ffea8b0d589f2
gauravsing <gauravmandy2@gmail.com>
2014-02-26 11:56:23 +0530

    Aahar fair
    aahar fair application

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<div data-role="header" data-position="fixed">
+			
+                 <h1>AAHAR 2014</h1>
+      <a href="index.html#a" data-icon="home" data-transition="flow" >Home</a> <a href="#inside-a" data-icon="bars" >Menu</a>
+      <div data-role="navbar">
+        <ul>
+          <li><a href="#about" data-icon="info" data-theme="b" data-transition="slide">ABOUT AAHAR</a></li>
+          <li><a href="#fair_fact" data-icon="star" data-theme="b" data-transition="slide">FAIR FACTS</a></li>
+          <li><a href="#organiser" data-icon="phone" data-theme="b" data-transition="slide">ORGANISER</a></li>
+          <li><a href="venue.html" data-icon="location" data-theme="b" data-transition="slide">VENUE</a></li>
+        </ul>
+      </div>
+			</div>
